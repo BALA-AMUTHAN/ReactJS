40896e800ed8ba1a11994585c0a77513a631325d
unknown <suryastranger9@gmail.com>
2023-06-26 16:39:30 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.module.css';
+import ProfileList from './components/profile/ProfileList';
+
+
+function App() {
+  return (
+    <div >
+      <ProfileList/>
+    </div>
+  );
+}
+
+export default App;
